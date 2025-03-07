a786a790000abb46ae9b5628bed6ce17242e277a
Raunakk18 <raunak654>
2025-03-07 20:48:37 +0530

    modified script.js

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-// ----------------Animation Typing------------------//
+// Purpose: This file is used to write the javascript code for the website.
 var typed = new Typed(".typing", {
   strings: ["A Web Developer", "A Web Designer"],
   typeSpeed: 100,
   backSpeed: 80,
   loop: true
-
 });
 
+
+
+
 var sidemenu = document.getElementById("sidemenu");
+
 function openmenu() {
-  document.querySelector('nav').classList.add('nav-show')
+  document.querySelector('nav').classList.add('nav-show');
 }
+
 function closemenu() {
-  document.querySelector('nav').classList.remove('nav-show')
+  document.querySelector('nav').classList.remove('nav-show');
 }
-const links = document.querySelectorAll('#sidemen li a')
 
-links.forEach(d=>{
-   d.addEventListener('click', ()=>{
-       document.querySelector('nav').classList.remove('nav-show')
+const links = document.querySelectorAll('#sidemenu li a'); 
+
+links.forEach(d => {
+  d.addEventListener('click', () => {
+    document.querySelector('nav').classList.remove('nav-show')
+  })  
 })
-})
-
-
